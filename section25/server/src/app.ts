68334f012c2e381243608ec2d29d0e0d519ad653
Luca Pennini <luca198871@gmail.com>
2023-10-27 14:13:14 +0200

    quicksave 381

--- section25/server/src/app.ts
+++ section25/server/src/app.ts
@@ -12,7 +12,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
-console.log(path.join(__dirname, "images"));
 
 // middleware to avoid the CORS policy error
 app.use((req, res, next) => {

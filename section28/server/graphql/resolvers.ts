fc31b6b8567c5984d97177da2105e2d80beeecf8
Luca Pennini <luca198871@gmail.com>
2023-11-01 12:56:08 +0100

    quicksave 425 w apollo instead of graphql express

--- section28/server/graphql/resolvers.ts
+++ section28/server/graphql/resolvers.ts
@@ -1,14 +1,21 @@
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import validator from "validator";
 import User from "../models/user";
 import Utils from "../utils/utils";
+import { ExtendedError } from "../types/types";
 
 type UserInputData = {
   email: string;
   name: string;
   password: string;
 };
+
+type Arguments = {
+  userInput: UserInputData;
+};
+
 export const resolvers = {
   Query: {
     users: async () => {
@@ -20,14 +27,25 @@ export const resolvers = {
   },
 
   Mutation: {
-    createUser: async (_, { userInput }, req) => {
-      const ui: UserInputData = userInput;
-      const email = ui.email;
+    createUser: async (_, { userInput }: Arguments) => {
+      const errors = [];
+      if (!validator.isEmail(userInput.email))
+        errors.push({ message: "Invalid e-mail" });
+      if (
+        validator.isEmpty(userInput.password) ||
+        //validator.trim(userInput.password) ||
+        !validator.isLength(userInput.password, { min: 8, max: 20 })
+      )
+        errors.push({ message: "Password is too short" });
+      if (validator.isEmpty(userInput.name))
+        errors.push({ message: "Invalid name" });
+      if (errors.length > 0) throw new Error("Invalid Input");
+      const email = userInput.email;
       const existingUser = await User.findOne({ email });
       if (existingUser)
         Utils.throwNewError("An user with that email already exists", 422);
-      const password = await bcrypt.hash(ui.password, 12);
-      const name = ui.name;
+      const password = await bcrypt.hash(userInput.password, 12);
+      const name = userInput.name;
       const user = new User({ email, name, password });
       const createdUser = await user.save();
       return { ...createdUser._doc, _id: createdUser._id.toString() };

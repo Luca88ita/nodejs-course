2d6e98e1062fcc9e71628a7114d1b8f4886f7845
Luca Pennini <luca198871@gmail.com>
2023-11-11 21:37:28 +0100

    section35 fist setup

---
+++ section35/server/app.ts
@@ -0,0 +1,32 @@
+/* const text = "Prova";
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+const data = encoder.encode(text);
+
+Deno.writeFile("message.txt", data).then(() => console.log("message saved"));
+
+Deno.readFile("message.txt").then((res) => console.log(decoder.decode(res)));
+ */
+
+/* Deno.serve({ port: 8080 }, (_req) => {
+  return new Response("Hello, World!");
+});
+ */
+
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import todoRoutes from "./routes/todos.ts";
+
+const app = new Application();
+
+app.use(async (_ctx, next) => {
+  console.log(
+    "This fake middleware MUST be async for being used correctly with all the following ones, and the next must be awaited"
+  );
+  await next();
+});
+
+app.use(todoRoutes.routes());
+app.use(todoRoutes.allowedMethods());
+
+await app.listen({ port: 8080 });
